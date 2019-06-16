e55a4d2b4da83a4aaf9faec11d66d4b768540a55
leostin <leostin@itcast.com>
2019-06-16 21:14:38 +0800

    这是一个测试工程

---
+++ AAA/src/com/itheima/Git_Test.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class Git_Test {
+    public static void main(String[] args) {
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+        System.out.println("hello git test");
+    }
+}
